909ecf89f7670cc78c18c6aa77cc1c681ac9a10e
shruti shanklesha <shruti07092006@gmail.com>
2026-02-10 08:45:22 +0530

    merged all files

--- addition.py
+++ addition.py
@@ -1,2 +1,2 @@
 def add(a, b):
-    sum = a + b
+    return a + b
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #calculator 
+
+import addition as a
+import subtraction as s
+import multi as m
+
+print(" addition results = ", a.add(5,6))
+print(" subtraction results = ", s.sub(5,6))
+print(" multiplication results = ", m.mul(5,6))
